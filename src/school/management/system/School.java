a06fe79b1f240255f18142e4e85b9806e30d5b28
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-22 19:35:31 -0400

    init School class

---
+++ src/school/management/system/School.java
@@ -0,0 +1,13 @@
+package school.management.system;
+
+/**
+ * Many teachers, many students.
+ */
+
+public class School {
+
+    private Teacher teacher;
+    private Student student;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
+}
