5c306414af6d521bff7cbbdf40906c9a3b06eb4b
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-22 19:43:37 -0400

    School - List for Teacher and STudent

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -1,13 +1,16 @@
 package school.management.system;
 
+import java.util.List;
+
 /**
  * Many teachers, many students.
+ * Implement teachers and students using ArrayList
  */
 
 public class School {
 
-    private Teacher teacher;
-    private Student student;
+    private List<Teacher> teachers;
+    private List<Student> students;
     private int totalMoneyEarned;
     private int totalMoneySpent;
 }
