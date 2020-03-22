861bc6983f7ffd288faa9fbc402ecf222c19123e
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-22 19:06:10 -0400

    init Teacher class

---
+++ src/school/management/system/Teacher.java
@@ -0,0 +1,13 @@
+package school.management.system;
+
+/**
+ * This class is responsible for keeping track
+ * of teacher's name, id, salary.
+ */
+
+public class Teacher {
+
+    private int id;
+    private String name;
+    private int salary;
+}
