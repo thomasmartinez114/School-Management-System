e0327d72cb384a48c1d44ad061116a45a242002a
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-22 19:10:59 -0400

    Teacher method

--- src/school/management/system/Teacher.java
+++ src/school/management/system/Teacher.java
@@ -10,4 +10,17 @@ public class Teacher {
     private int id;
     private String name;
     private int salary;
+
+
+    /**
+     * Creates a new Teacher object.
+     * @param id id for the teacher.
+     * @param name name of the teacher.
+     * @param salary salary of the teacher.
+     */
+    public Teacher(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
 }
