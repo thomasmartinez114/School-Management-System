48bd98b626bc37ffc4bf6136559ac7d46dd98000
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-22 02:36:05 -0400

    setGrade()

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -14,16 +14,24 @@ public class Student {
 
     /**
      * To create a new student by initializing:
+     * Fee per student is $30,000
+     * Fees paid initially is $0
      * @param id
      * @param name
      * @param grade
      */
 
     public Student(int id, String name, int grade) {
-
+        feesPaid = 0;
+        this.feesTotal = 30000;
         this.id = id;
         this.name = name;
         this.grade = grade;
-
     }
+    // Not going to alter student's name, student's id.
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
 }
