8e5ea27583cb35d52438b8586fe1f3520ef9353b
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-22 02:28:13 -0400

    initialized Student class

---
+++ src/school/management/system/Student.java
@@ -0,0 +1,16 @@
+package school.management.system;
+
+/*
+This class is responsible for keeping track of
+Student fees, name, grade & fees paid
+ */
+
+public class Student {
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+    
+}
