a0f835af4554ae652a62ff5c1c8139f3ed69ca24
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-22 22:20:20 -0400

    Added @Override - to show the object of student and not a random string

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -99,4 +99,10 @@ public class Student {
     public int getRemainingFees() {
         return feesTotal - feesPaid;
     }
+
+    @Override
+    public String toString() {
+        return "Student's name : " + name +
+                ". Total fees paid so far $" + feesPaid;
+    }
 }
