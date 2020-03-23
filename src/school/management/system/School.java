74d5c2558b64eb9f8a22b1b291814bc4782c1144
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-22 20:47:25 -0400

    School List for Teacher and Student

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -13,4 +13,16 @@ public class School {
     private List<Student> students;
     private int totalMoneyEarned;
     private int totalMoneySpent;
+
+    /**
+     * new school object is created.
+     * @param teachers list of teachers in the school.
+     * @param students list of students in the school.
+     */
+    public School (List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent = 0;
+    }
 }
