e0c2ff4b4dd0aa4cd4e98c228618b952a7d8fe8b
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-22 21:17:41 -0400

    init Main & created 3 teachers

---
+++ src/school/management/system/Main.java
@@ -0,0 +1,9 @@
+package school.management.system;
+
+public class Main {
+    public static void main(String[] args) {
+        Teacher lizzy = new Teacher(1, "Lizzy", 500);
+        Teacher melissa = new Teacher(2, "Melissa", 700);
+        Teacher vanderhorn = new Teacher(3, "Vanderhorn", 600);
+    }
+}
