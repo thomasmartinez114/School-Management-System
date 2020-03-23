f0ffef406de3a6e6c6834d9aa7a557e5a783e121
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-22 21:35:27 -0400

    created students

--- src/school/management/system/Main.java
+++ src/school/management/system/Main.java
@@ -5,5 +5,9 @@ public class Main {
         Teacher lizzy = new Teacher(1, "Lizzy", 500);
         Teacher melissa = new Teacher(2, "Melissa", 700);
         Teacher vanderhorn = new Teacher(3, "Vanderhorn", 600);
+
+        Student tamasha = new Student(1, "Tamasha", 4);
+        Student rakshith = new Student(2, "Rakshith", 12);
+        Student thomas = new Student(3, "Thomas", 5);
     }
 }
